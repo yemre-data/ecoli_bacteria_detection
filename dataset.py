7294129b6f686e7d25af6fb94a062cea86bf05b3
yemre-data <yemrecelik2525@gmail.com>
2021-11-22 11:25:03 +0100

    creating data set and import libraries

---
+++ dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import cv2
+import numpy as np
+import torch
+import json
+import os
+from PIL import Image
+
+
+
+
+
+
+
+class EcoliBacteriaDataset(Dataset):
--- ssd.py
+++ ssd.py
@@ -1,5 +1,4 @@
 from torch import nn
-from utils import *
 import torch.nn.functional as F
 from math import sqrt
 from itertools import product as product
