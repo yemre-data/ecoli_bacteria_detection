75f4e146daa90420db6e187defa63a3e1f8dee17
yemre-data <yemrecelik2525@gmail.com>
2021-11-22 15:26:57 +0100

    for loop bug solved

--- dataset.py
+++ dataset.py
@@ -9,6 +9,7 @@ from PIL import Image
 def crop_image(dir_im,dir_save):
     im = cv2.imread(dir_im)
     im_height, im_width = im.shape[0], im.shape[1]
+    print("ok")
     if im_width%300 == 0:
         nm_total_image = im_width/300
         nm_total_image = int(nm_total_image)
@@ -28,34 +29,34 @@ def crop_image(dir_im,dir_save):
     else:
         nm_total_image = im_width/300
         nm_total_image = int(nm_total_image)+1
-    for i in range(0,nm_total_image):
-        dir_save = dir_save + "/" + str(i) + "_.tif"
-        x, y = 0, 0
-        width, height = 300, 300
-        crop_im = im[y:y + height, x:x + width]
+        for i in range(0,nm_total_image):
+            dir_save = dir_save + "/" + str(i) + "_.tif"
+            x, y = 0, 0
+            width, height = 300, 300
+            crop_im = im[y:y + height, x:x + width]
+
+            if im_height < 300:
+                padding_amount = 300 - im_height
+                im_pad = cv2.copyMakeBorder(crop_im, 0, padding_amount, 0, 0, cv2.BORDER_CONSTANT, (0, 0, 0))
+                cv2.imwrite(dir_save, im_pad)
+            else:
+                cv2.imwrite(dir_save, crop_im)
+            x += 300
 
         if im_height < 300:
+            dir_save = dir_save + "/_last.tif"
+            x, y = im_width-300, 0
+            width, height = 300, 300
+            crop_im = im[y:y + height, x:x + width]
             padding_amount = 300 - im_height
             im_pad = cv2.copyMakeBorder(crop_im, 0, padding_amount, 0, 0, cv2.BORDER_CONSTANT, (0, 0, 0))
             cv2.imwrite(dir_save, im_pad)
         else:
+            dir_save = dir_save + "/_last.tif"
+            x, y = im_width - 300, 0
+            width, height = 300, 300
+            crop_im = im[y:y + height, x:x + width]
             cv2.imwrite(dir_save, crop_im)
-        x += 300
-
-    if im_height < 300:
-        dir_save = dir_save + "/_last.tif"
-        x, y = im_width-300, 0
-        width, height = 300, 300
-        crop_im = im[y:y + height, x:x + width]
-        padding_amount = 300 - im_height
-        im_pad = cv2.copyMakeBorder(crop_im, 0, padding_amount, 0, 0, cv2.BORDER_CONSTANT, (0, 0, 0))
-        cv2.imwrite(dir_save, im_pad)
-    else:
-        dir_save = dir_save + "/_last.tif"
-        x, y = im_width - 300, 0
-        width, height = 300, 300
-        crop_im = im[y:y + height, x:x + width]
-        cv2.imwrite(dir_save, crop_im)
 
 #first
 crop_image("/home/criuser/Desktop/Internship/Original-s1-ss-286-roi-24.tif","home/criuser/Desktop/Internship/Cropped_1")
