43f2b709068c37ea58cfe50ea175f7f2bff43918
yemre-data <yemrecelik2525@gmail.com>
2021-11-23 17:20:24 +0100

    bug fixed x,y

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import cv2
 import numpy as np
@@ -5,20 +6,20 @@ import torch
 import json
 import os
 from PIL import Image
-
+import matplotlib.pyplot as plt
 
 def crop_image(dir_im, dir_save):
-    im = cv2.imread(dir_im)
-    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    im = cv2.imread(dir_im,-1)
+    #im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     im_height, im_width = im.shape[0], im.shape[1]
     root_dir = dir_save
     if im_width % 300 == 0:
         nm_total_image = im_width / 300
         nm_total_image = int(nm_total_image)
+        x, y = 0, 0
         for i in range(0, nm_total_image):
             name = "P" + str(i) + ".tif"
             dir_save = os.path.join(os.path.expanduser('~'), root_dir, name)
-            x, y = 0, 0
             width, height = 300, 300
             crop_im = im[y:y + height, x:x + width]
             if im_height < 300:
@@ -31,10 +32,10 @@ def crop_image(dir_im, dir_save):
     else:
         nm_total_image = im_width / 300
         nm_total_image = int(nm_total_image) + 1
+        x, y = 0, 0
         for i in range(1, nm_total_image):
             name = "P" + str(i) + ".tif"
             dir_save = os.path.join(os.path.expanduser('~'), root_dir, name)
-            x, y = 0, 0
             width, height = 300, 300
             crop_im = im[y:y + height, x:x + width]
             if im_height < 300:
@@ -44,9 +45,9 @@ def crop_image(dir_im, dir_save):
             else:
                 cv2.imwrite(dir_save, crop_im)
             x += 300
+            print(x)
 
         if im_height < 300:
-
             name = "P" + str(nm_total_image) + ".tif"
             dir_save = os.path.join(os.path.expanduser('~'), root_dir, name)
             x, y = im_width - 300, 0
