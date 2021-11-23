c33341f88ec88e519b66443e6e6009a9435703ff
yemre-data <yemrecelik2525@gmail.com>
2021-11-23 11:03:14 +0100

    BGR to RGB converted

--- dataset.py
+++ dataset.py
@@ -6,15 +6,17 @@ import json
 import os
 from PIL import Image
 
-def crop_image(dir_im,dir_save):
+
+def crop_image(dir_im, dir_save):
     im = cv2.imread(dir_im)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     im_height, im_width = im.shape[0], im.shape[1]
     root_dir = dir_save
-    if im_width%300 == 0:
-        nm_total_image = im_width/300
+    if im_width % 300 == 0:
+        nm_total_image = im_width / 300
         nm_total_image = int(nm_total_image)
         for i in range(0, nm_total_image):
-            name = "P"+str(i) + ".tif"
+            name = "P" + str(i) + ".tif"
             dir_save = os.path.join(os.path.expanduser('~'), root_dir, name)
             x, y = 0, 0
             width, height = 300, 300
@@ -27,9 +29,9 @@ def crop_image(dir_im,dir_save):
                 cv2.imwrite(dir_save, crop_im)
             x += 300
     else:
-        nm_total_image = im_width/300
-        nm_total_image = int(nm_total_image)+1
-        for i in range(1,nm_total_image):
+        nm_total_image = im_width / 300
+        nm_total_image = int(nm_total_image) + 1
+        for i in range(1, nm_total_image):
             name = "P" + str(i) + ".tif"
             dir_save = os.path.join(os.path.expanduser('~'), root_dir, name)
             x, y = 0, 0
@@ -47,7 +49,7 @@ def crop_image(dir_im,dir_save):
 
             name = "P" + str(nm_total_image) + ".tif"
             dir_save = os.path.join(os.path.expanduser('~'), root_dir, name)
-            x, y = im_width-300, 0
+            x, y = im_width - 300, 0
             width, height = 300, 300
             crop_im = im[y:y + height, x:x + width]
             padding_amount = 300 - im_height
@@ -62,13 +64,12 @@ def crop_image(dir_im,dir_save):
             cv2.imwrite(dir_save, crop_im)
 
 
-crop_image('/home/criuser/Desktop/Internship/Original-s20-ss-382-roi-24.tif','Desktop/Internship/Cropped_20' )
+crop_image('/home/criuser/Desktop/Internship/Original-s1-ss-286-roi-24.tif', 'Desktop/Internship/Cropped_1')
+
+# bbox_1 = pd.read_csv("/home/criuser/Desktop/Internship/1.csv")
+
+# bbox_1 = bbox_1.loc[bbox_1['Slice'] != 0]
 
 
-#bbox_1 = pd.read_csv("/home/criuser/Desktop/Internship/1.csv")
-
-#bbox_1 = bbox_1.loc[bbox_1['Slice'] != 0]
-
-
-#print(len(bbox_1))
-#class EcoliBacteriaDataset(Dataset):
+# print(len(bbox_1))
+# class EcoliBacteriaDataset(Dataset):
