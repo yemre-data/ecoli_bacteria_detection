ce13d5ba1a89273815ec77110695799a81d22002
yemre-data <yemrecelik2525@gmail.com>
2021-11-23 10:38:47 +0100

    solved cropped function and it works well

--- dataset.py
+++ dataset.py
@@ -9,13 +9,13 @@ from PIL import Image
 def crop_image(dir_im,dir_save):
     im = cv2.imread(dir_im)
     im_height, im_width = im.shape[0], im.shape[1]
-    DIR_SAVE =  dir_save
-
+    root_dir = dir_save
     if im_width%300 == 0:
         nm_total_image = im_width/300
         nm_total_image = int(nm_total_image)
         for i in range(0, nm_total_image):
-            dir_save = dir_save + "/" + str(i) + ".tif"
+            name = "P"+str(i) + ".tif"
+            dir_save = os.path.join(os.path.expanduser('~'), root_dir, name)
             x, y = 0, 0
             width, height = 300, 300
             crop_im = im[y:y + height, x:x + width]
@@ -26,13 +26,12 @@ def crop_image(dir_im,dir_save):
             else:
                 cv2.imwrite(dir_save, crop_im)
             x += 300
-            dir_save = DIR_SAVE
-
     else:
         nm_total_image = im_width/300
         nm_total_image = int(nm_total_image)+1
         for i in range(1,nm_total_image):
-            dir_save = dir_save + "/" + str(i) + ".tif"
+            name = "P" + str(i) + ".tif"
+            dir_save = os.path.join(os.path.expanduser('~'), root_dir, name)
             x, y = 0, 0
             width, height = 300, 300
             crop_im = im[y:y + height, x:x + width]
@@ -43,11 +42,11 @@ def crop_image(dir_im,dir_save):
             else:
                 cv2.imwrite(dir_save, crop_im)
             x += 300
-            dir_save = DIR_SAVE
 
         if im_height < 300:
-            dir_save = DIR_SAVE
-            dir_save = dir_save + "/last.tif"
+
+            name = "P" + str(nm_total_image) + ".tif"
+            dir_save = os.path.join(os.path.expanduser('~'), root_dir, name)
             x, y = im_width-300, 0
             width, height = 300, 300
             crop_im = im[y:y + height, x:x + width]
@@ -55,18 +54,17 @@ def crop_image(dir_im,dir_save):
             im_pad = cv2.copyMakeBorder(crop_im, 0, padding_amount, 0, 0, cv2.BORDER_CONSTANT, (0, 0, 0))
             cv2.imwrite(dir_save, im_pad)
         else:
-            dir_save = DIR_SAVE
-            dir_save = dir_save + "/last.tif"
+            name = "P" + str(nm_total_image) + ".tif"
+            dir_save = os.path.join(os.path.expanduser('~'), root_dir, name)
             x, y = im_width - 300, 0
             width, height = 300, 300
             crop_im = im[y:y + height, x:x + width]
             cv2.imwrite(dir_save, crop_im)
 
-#first
-crop_image('/home/criuser/Desktop/Internship/Original-s1-ss-286-roi-24.tif','home/criuser/Desktop/Internship/Cropped_1' )
 
-a = cv2.imread('/home/criuser/Desktop/Internship/Original-s1-ss-286-roi-24.tif')
-cv2.imwrite('/home/criuser/Desktop/a.tif',a)
+crop_image('/home/criuser/Desktop/Internship/Original-s20-ss-382-roi-24.tif','Desktop/Internship/Cropped_20' )
+
+
 #bbox_1 = pd.read_csv("/home/criuser/Desktop/Internship/1.csv")
 
 #bbox_1 = bbox_1.loc[bbox_1['Slice'] != 0]
