fc3ac288e57de3c85a2934f03a3ba31c3d2982ac
yemre-data <yemrecelik2525@gmail.com>
2021-12-01 09:53:29 +0100

    figure it out that cv2 read just first slice of multiple tif image , so I change data set that is taking first slice data for now.

--- utils.py
+++ utils.py
@@ -38,7 +38,7 @@ def create_images_and_bbox_list(dir_image_folder,dir_csv_folder, dir_save):
         im = cv2.imread(dir_image, -1)
         im_height, im_width = im.shape[0], im.shape[1]
         bbox_df = pd.read_csv(dir_bbox)
-        bbox_df = bbox_df.loc[bbox_df['Slice'] != 0]
+        bbox_df = bbox_df.loc[bbox_df['Slice'] == 1]
         bbox_df = bbox_df[['BX', 'BY', 'Width', 'Height']]
         bbox_df = {"xmin": bbox_df['BX'], "ymin": bbox_df['BY'], "xmax": bbox_df['BX'] + bbox_df['Width'],
                   "ymax": bbox_df['BY'] + bbox_df['Height']}
@@ -168,7 +168,29 @@ def create_images_and_bbox_list(dir_image_folder,dir_csv_folder, dir_save):
     print('\nCropped original images and created %d train and test images. Images has been saved to %s .' % (total_image-1,root_dir))
     print('\nFound %d bounding boxes. Image path and bbox locations has ben saved to %s' % (n_box,root_dir))
 
-create_images_and_bbox_list("/home/criuser/Desktop/Internship/Orginal_images", '/home/criuser/Desktop/Internship/Orginal_measure','/home/criuser/Desktop/Internship/Output')
+#create_images_and_bbox_list("/home/criuser/Desktop/Internship/Orginal_images", '/home/criuser/Desktop/Internship/Orginal_measure','/home/criuser/Desktop/Internship/Output')
+def display_image(dir_json_images,dir_json_bbx,n_display):
+    with open(dir_json_images) as f:
+        dir_images = json.load(f)
+    with open(dir_json_bbx) as f:
+        boxes = json.load(f)
+
+    count = 0
+    for i,b in zip(dir_images, boxes):
+        img = cv2.imread(i)
+        bboxes = b["boxes"]
+
+        for j in bboxes:
+            img = cv2.rectangle(img, (j[0], j[1]), (j[2], j[3]), (255, 0, 0), 1)
+        count += 1
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        plt.imshow(img)
+        plt.show()
+        if count == n_display:
+            break
+
+
+display_image('/home/criuser/Desktop/Internship/Output/Images.json','/home/criuser/Desktop/Internship/Output/Bboxes.json',2)
 
 
 
